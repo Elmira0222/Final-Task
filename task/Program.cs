488a2b6c6c014d238b61400570190ce35d916cd1
Elmira <zhan0222@mail.ru>
2023-01-27 22:22:08 +0600

    added task

---
+++ task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Given an array of strings: ");
+string[] array = {"3456", ":-)", "son",  "Astana", "==", "2(n^2)", "-4"};
+for (int i = 0; i < array.Length; i++)
+Console.Write(array[i] + " ");
+Console.WriteLine();
+Console.WriteLine("Final array: ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
